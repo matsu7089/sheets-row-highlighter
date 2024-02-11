c373fa67572ee822b53bc565ad6b345f1edd1975
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2024-02-11 15:17:28 +0900

    add: types.jsを追加

---
+++ scripts/content/types.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+/**
+ * @typedef {Object} HighlightRect
+ * @property {number} x
+ * @property {number} y
+ * @property {number} width
+ * @property {number} height
+ */
+
+/**
+ * @typedef {Object} ActiveCellLocator
+ * @property {() => Array<HighlightRect>} getHighlightRectList
+ * @property {() => Partial<CSSStyleDeclaration>} getSheetContainerStyle
+ */
