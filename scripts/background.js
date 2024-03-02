4e878e790b0f83a95c41c4909ef27dae9b0d60ef
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2024-03-02 15:13:29 +0900

    update: auto時のショートカット入力処理を追加

--- scripts/background.js
+++ scripts/background.js
@@ -1,32 +1,62 @@
+const sendMessageToActiveTab = async (message) => {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  })
+
+  return chrome.tabs.sendMessage(tab.id, message)
+}
+
 const defaultRow = true
 const defaultColumn = false
 
-chrome.commands.onCommand.addListener((command) => {
-  chrome.storage.local.get(['row', 'column'], (items) => {
-    let row = items.row === undefined ? defaultRow : items.row
-    let column = items.column === undefined ? defaultColumn : items.column
+chrome.commands.onCommand.addListener(async (command) => {
+  const sheetKey = await sendMessageToActiveTab({ type: 'getSheetKey' }).catch(
+    () => {}
+  )
 
-    // 入力されたショートカットに応じて設定切り替え
-    switch (command) {
-      case 'toggleRow':
-        row = !row
-        break
-      case 'toggleColumn':
-        column = !column
-        break
+  chrome.storage.local.get(
+    ['row', 'column', 'auto', 'sheetSettingsMap'],
+    (items) => {
+      let row = items.row ?? defaultRow
+      let column = items.column ?? defaultColumn
+      const sheetSettingsMap = items.sheetSettingsMap ?? {}
+
+      // 入力されたショートカットに応じて設定切り替え
+      switch (command) {
+        case 'toggleRow': {
+          row = !row
+          break
+        }
+        case 'toggleColumn': {
+          column = !column
+          break
+        }
+      }
+
+      if (sheetKey && items.auto && sheetSettingsMap[sheetKey]) {
+        Object.assign(sheetSettingsMap[sheetKey], {
+          row,
+          column,
+          lastAccess: Date.now(),
+        })
+      }
+
+      // 設定保存
+      chrome.storage.local.set({
+        row,
+        column,
+        sheetSettingsMap,
+      })
+
+      // popupにメッセージを送信
+      chrome.runtime
+        .sendMessage({
+          type: 'commands',
+          row,
+          column,
+        })
+        .catch(() => {})
     }
-
-    // 設定保存
-    chrome.storage.local.set({
-      row,
-      column,
-    })
-
-    // メッセージを送信
-    chrome.runtime.sendMessage({
-      type: 'commands',
-      row,
-      column,
-    })
-  })
+  )
 })
